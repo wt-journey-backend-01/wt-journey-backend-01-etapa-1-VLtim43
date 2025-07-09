08eb48c26626d6f5709b7e49e1db9c20f8aa8a1c
VLtim43 <fernandoreal.valentim@gmail.com>
2025-07-08 21:01:07 -0300

    update: home page

--- devburguer/src/server/server.js
+++ devburguer/src/server/server.js
@@ -1,10 +1,28 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
+const root = path.resolve(__dirname, "..");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(root, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(root, "views", "index.html"));
+});
+
+app.get("/contato", (req, res) => {
+  res.sendFile(path.join(root, "views", "contato.html"));
+});
+
+app.get("/api/lanches", (req, res) => {
+  const lanches = require(path.join(root, "public", "data", "lanches.json"));
+  res.json(lanches);
+});
+
+app.use((req, res) => {
+  res.status(404).send("Página não encontrada. <a href='/'>Voltar</a>");
 });
 
 app.listen(PORT, () => {
